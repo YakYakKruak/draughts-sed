f316c1a5f5140dbd65be0f6aab28aecd9f5fa49d
YakYakKruak <golovanev98@mail.ru>
2018-05-26 12:24:39 +0300

    refactored

--- draughts.sh
+++ draughts.sh
@@ -1,45 +1,42 @@
 #!/bin/bash
 
-desk="a8b8c8d8e8f8g8h8
-a7b7c7d7e7f7g7h7
-a6b6c6d6e6f6g6h6
-a5b5c5d5e5f5g5h5
-a4b4c4d4e4f4g4h4
-a3b3c3d3e3f3g3h3
-a2b2c2d2e2f2g2h2
-a1b1c1d1e1f1g1h1
+desk="a81 b8 c81 d8 e81 f8 g81 h8 
+a7 b71 c7 d71 e7 f71 g7 h71 
+a61 b6 c61 d6 e61 f6 g61 h6 
+a5 b5 c5 d5 e5 f5 g5 h5 
+a4 b4 c4 d4 e4 f4 g4 h4 
+a3 b30 c3 d30 e3 f30 g3 h30 
+a20 b2 c20 d2 e20 f2 g20 h2 
+a1 b10 c1 d10 e1 f10 g1 h10 
 Input: "
 
 esc=`printf "\033[0m"`
 blackwhite=`printf "\033[40;1m"`
+black=`printf "\033[40m"`
 white=`printf "\033[47m"`
 blackblack=`printf "\033[40;30;1m"`
 
 draw() {
     echo "$desk" | sed "
-    1s/\([aceg]8\)/${blackblack}\1${esc}/g
-    3s/\([aceg]6\)/${blackblack}\1${esc}/g
-    5s/\([aceg]4\)/${blackwhite}\1${esc}/g
-    7s/\([aceg]2\)/${blackwhite}\1${esc}/g
-    1s/\([bdfh]8\)/${white}\1${esc}/g
-    3s/\([bdfh]6\)/${white}\1${esc}/g
-    5s/\([bdfh]4\)/${white}\1${esc}/g
-    7s/\([bdfh]2\)/${white}\1${esc}/g
-    2s/\([aceg]7\)/${white}\1${esc}/g
-    4s/\([aceg]5\)/${white}\1${esc}/g
-    6s/\([aceg]3\)/${white}\1${esc}/g
-    8s/\([aceg]1\)/${white}\1${esc}/g
-    2s/\([bdfh]7\)/${blackblack}\1${esc}/g
-    4s/\([bdfh]5\)/${blackwhite}\1${esc}/g
-    6s/\([bdfh]3\)/${blackwhite}\1${esc}/g
-    8s/\([bdfh]1\)/${blackwhite}\1${esc}/g
-    1s/\([aceg]8\)/• /g
-    2s/\([bdfh]7\)/• /g
-    3s/\([aceg]6\)/• /g
-    6s/\([bdfh]3\)/• /g
-    7s/\([aceg]2\)/• /g
-    8s/\([bdfh]1\)/• /g
-    s/[a-z][1-8]/  /g"
+     1s/\([aceg]8 \)/${black}\1${esc}/g
+     3s/\([aceg]6 \)/${black}\1${esc}/g
+     5s/\([aceg]4 \)/${black}\1${esc}/g
+     7s/\([aceg]2 \)/${black}\1${esc}/g
+     1s/\([bdfh]8 \)/${white}\1${esc}/g
+     3s/\([bdfh]6 \)/${white}\1${esc}/g
+     5s/\([bdfh]4 \)/${white}\1${esc}/g
+     7s/\([bdfh]2 \)/${white}\1${esc}/g
+     2s/\([bdfh]7 \)/${black}\1${esc}/g
+     4s/\([bdfh]5 \)/${black}\1${esc}/g
+     6s/\([bdfh]3 \)/${black}\1${esc}/g
+     8s/\([bdfh]1 \)/${black}\1${esc}/g
+     2s/\([aceg]7 \)/${white}\1${esc}/g
+     4s/\([aceg]5 \)/${white}\1${esc}/g
+     6s/\([aceg]3 \)/${white}\1${esc}/g
+     8s/\([aceg]1 \)/${white}\1${esc}/g
+     s/[a-h][1-8]1 /${blackblack}• ${esc}/g
+     s/[a-h][1-8]0 /${blackwhite}• ${esc}/g
+     s/[a-z][1-8] /  /g"
 }
 
 move() {
@@ -53,6 +50,7 @@ move() {
             column_to=`printf %d "'${2:0:1}"`
             column_from=`expr $column_from - 97`
             column_to=`expr $column_to - 97`
+            echo "$column_from $column_to" > out.txt
             column_from=`expr 2 * $column_from`
             column_to=`expr 2 * $column_to`
             column_from=`expr $column_from + 1`
